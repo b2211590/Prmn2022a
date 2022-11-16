5bfb2884c9f9351cca72648303d936b394e28f17
b2211590 <b2211590@photon.chitose.ac.jp>
2022-11-16 13:16:59 +0900

    kadai3を追加しました

---
+++ src/lecture01/kadai3.java
@@ -0,0 +1,19 @@
+package lecture01;
+
+import java.util.Scanner;
+
+public class kadai3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input your age:");
+        int age = input.nextInt();
+
+        if(age < 20){
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        }else{
+            System.out.println("I am " + age + " years old so I can drink liquor.");
+        }
+    }
+}
