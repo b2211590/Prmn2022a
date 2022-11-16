ad382a0cda2bd3f56d6811640a8194e6abb8bd7e
b2211590 <b2211590@photon.chitose.ac.jp>
2022-11-16 12:59:46 +0900

    kadai2を追加しました

---
+++ src/lecture01/kadai2.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class kadai2 {
+    public static void main(String[] args) {
+        int studentNumber = 2211590;
+        System.out.println("b" + studentNumber + " Tamura Shuto");
+    }
+}
