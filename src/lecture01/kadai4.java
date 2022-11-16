6602a99fc78336dea1de3ff519d01825af5483d4
b2211590 <b2211590@photon.chitose.ac.jp>
2022-11-16 13:25:28 +0900

    kadai4を追加しました

---
+++ src/lecture01/kadai4.java
@@ -0,0 +1,22 @@
+package lecture01;
+
+public class kadai4 {
+    public static void main(String[] args) {
+
+        int[] array = new int[100];
+
+        int sum = 0;
+
+        for (int i = 0; i < 100; i++) {
+            array[i] = i + 1;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if(i % 2 == 0){
+                sum += array[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
