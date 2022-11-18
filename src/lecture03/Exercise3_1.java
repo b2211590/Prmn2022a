97ec3724aa6e1d0bb9965cc56fc9b54b4f79114a
b2211590 <b2211590@photon.chitose.ac.jp>
2022-11-18 22:57:02 +0900

    Exercise3_1を追加しました

--- src/lecture03/Exercise3_1.java
+++ src/lecture03/Exercise3_1.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Exercise3_1 {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
 
         System.out.print("任意の文字列を入力してください:");
